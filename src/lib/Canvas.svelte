a7fd52842fbc250fedc57c52d78ab5e5283abb7b
kaan.taskingenc <kaan.taskingenc@gmail.com>
2024-06-03 20:28:05 +0000

    feat: click to draw

--- src/lib/Canvas.svelte
+++ src/lib/Canvas.svelte
@@ -7,34 +7,59 @@
 
     // Grid settings
     const width = 800;
-    const height = 600;
+    const height = 800;
     const cellSize = 20;
     const rows = Math.floor(height / cellSize);
     const cols = Math.floor(width / cellSize);
+
+    // Initialize the grid
+    let grid: number[][] = [];
+
+    const initializeGrid = () => {
+      for (let row = 0; row < rows; row++) {
+        grid[row] = [];
+        for (let col = 0; col < cols; col++) {
+          grid[row][col] = 0; // Initialize all cells as dead
+        }
+      }
+    }
+
+     const drawGrid = () => {
+      for (let row = 0; row < rows; row++) {
+        for (let col = 0; col < cols; col++) {
+          context.beginPath()
+          context.rect(col * cellSize, row * cellSize, cellSize, cellSize);
+          context.fillStyle = grid[row][col] ? 'black' : 'white';
+          context.fill()
+          context.stroke()
+        }
+      }
+    }
+
+    // Function to handle canvas click
+    const handleCanvasClick = (event: MouseEvent) => {
+      const rect = canvas.getBoundingClientRect()
     
-    // Function to draw the grid
-    function drawGrid() {
-      for (let col = 0; col <= cols; col++) {
-        const x = col * cellSize
-        context.beginPath()
-        context.moveTo(x, 0)
-        context.lineTo(x, height)
-        context.stroke()
-      }
+      // clientX - left gives position from left side
+      // clientY - top gives position from top side
+      const x = event.clientX - rect.left
+      const y = event.clientY - rect.top
 
-      for (let row = 0; row <= rows; row++) {
-        const y = row * cellSize
-        context.beginPath()
-        context.moveTo(0, y)
-        context.lineTo(width, y)
-        context.stroke()
-      }
+      // By getting the x and y we positions we can divide it
+      // by cellSize to get the number of the cell likea a cordinate system
+      const col = Math.floor(x / cellSize)
+      const row = Math.floor(y / cellSize)
 
-  }
+      // Toggle alive or dead
+      grid[row][col] = grid[row][col] ? 0 : 1
+      drawGrid()
+    }
     
     onMount(() => {
       context = canvas.getContext('2d') as CanvasRenderingContext2D;
+      initializeGrid()
       drawGrid()
+      canvas.addEventListener('click', handleCanvasClick);
     });
   </script>
   
